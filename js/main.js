0c7e1d0258236ffdddc6a9f403bdd99b436c0a65
Roses_Nguyen <nngklinh.2911@gmail.com>
2026-03-13 01:25:56 -0400

    add more counter box

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { Counter } from "./modules/counter.js";
 import { StepCounter } from "./modules/counter.js";
 
-const counter1 = new Counter("#counter-container1", 8);
-const counter2 = new Counter("#counter-container2", 5);
-const counter3 = new StepCounter("#counterCon", 5, 10);
+const counter1 = new Counter("#counter-container1", 0);
+const counter2 = new Counter("#counter-container1", 6);
+const counter3 = new Counter("#counter-container2", 8);
+const counter4 = new StepCounter("#counterCon", 5, 10);
 
 // const step10 = new StepCounter(id, starting number, step);
