e97ae459f22f6ab51eae949a93d5072d36255686
Roses_Nguyen <nngklinh.2911@gmail.com>
2026-03-13 01:30:05 -0400

    change id of each counter to develop sass

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import { Counter } from "./modules/counter.js";
 import { StepCounter } from "./modules/counter.js";
 
 const counter1 = new Counter("#counter-container1", 0);
-const counter2 = new Counter("#counter-container1", 6);
-const counter3 = new Counter("#counter-container2", 8);
-const counter4 = new StepCounter("#counterCon", 5, 10);
+const counter2 = new Counter("#counter-container2", 6);
+const counter3 = new Counter("#counter-container3", 8);
+const counter4 = new StepCounter("counter-container4", 5, 10);
 
 // const step10 = new StepCounter(id, starting number, step);
